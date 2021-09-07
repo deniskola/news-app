1950bfc01ac87f857868631faeb8bbfefa55f7cd
Denis Kolaj <deniskolaj2@gmail.com>
2021-09-07 23:36:19 +0200

    add appbar/ header

---
+++ my-app/src/Components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {AppBar, Toolbar} from "@material-ui/core";
+
+export default function Header() {
+  return (
+    <AppBar>
+      <Toolbar className="toolbar"></Toolbar>
+    </AppBar>
+  );
+}
